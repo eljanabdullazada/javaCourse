b51285b593f04e04b89ed46752b1c08f54d0ecc0
eljan_abdullazada <a3371977@gmail.com>
2025-08-12 09:39:19 +0400

    Complete printing row of matrix

---
+++ src/lesson4/ArrayClassTask3.java
@@ -0,0 +1,17 @@
+package lesson4;
+
+public class ArrayClassTask3 {
+    public static void main(String[] args) {
+
+        int [][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for(int i = 0; i < matrix[1].length; i++){
+            System.out.print(matrix[1][i] + " ");
+        }
+
+    }
+}
