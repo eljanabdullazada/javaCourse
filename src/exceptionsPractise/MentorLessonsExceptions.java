c424e5582f3f906508527492269a54900343da38
eljan_abdullazada <a3371977@gmail.com>
2025-08-19 19:35:27 +0400

    integer parse int related infomraiton

---
+++ src/exceptionsPractise/MentorLessonsExceptions.java
@@ -0,0 +1,16 @@
+package exceptionsPractise;
+import java.util.Scanner;
+
+public class MentorLessonsExceptions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");
+
+        String age = scanner.nextLine();
+
+        // this is avaiting for an interger that is in string format
+        int ageStr = Integer.parseInt(age);
+        System.out.println(ageStr);
+
+    }
+}
