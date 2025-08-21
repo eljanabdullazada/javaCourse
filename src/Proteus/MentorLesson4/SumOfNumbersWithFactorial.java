dde5ba268a70f4d49f344eb022b7e49af97ed7b0
eljan_abdullazada <a3371977@gmail.com>
2025-08-21 20:44:43 +0400

    sum of number like factorial  method

--- src/Proteus/MentorLesson4/SumOfNumbersWithFactorial.java
+++ src/Proteus/MentorLesson4/SumOfNumbersWithFactorial.java
@@ -1,15 +1,17 @@
 package Proteus.MentorLesson4;
 import java.util.Scanner;
 
-public class FactorialTest {
+public class SumOfNumbersWithFactorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        System.out.println("Factorial of " + number + " is " + findFactorial(number));
+        System.out.println("Sum of all " + number + " numbers is " + findFactorial(number));
     }
 
     public static int findFactorial(int number){
-        if(number == 0 || number == 1) return 1;
-        return number * findFactorial(number - 1);
+        if(number == 0) return 0;
+        if(number == 1) return 1;
+
+        return number + findFactorial(number - 1);
     }
 }
