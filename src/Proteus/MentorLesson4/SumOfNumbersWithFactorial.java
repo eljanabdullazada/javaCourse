69a0b02b3f8ad25280f6be359f692376687ae39c
eljan_abdullazada <a3371977@gmail.com>
2025-08-21 20:47:12 +0400

    fix method name

--- src/Proteus/MentorLesson4/SumOfNumbersWithFactorial.java
+++ src/Proteus/MentorLesson4/SumOfNumbersWithFactorial.java
@@ -5,13 +5,13 @@ public class SumOfNumbersWithFactorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        System.out.println("Sum of all " + number + " numbers is " + findFactorial(number));
+        System.out.println("Sum of all " + number + " numbers is " + findSum(number));
     }
 
-    public static int findFactorial(int number){
+    public static int findSum(int number){
         if(number == 0) return 0;
         if(number == 1) return 1;
 
-        return number + findFactorial(number - 1);
+        return number + findSum(number - 1);
     }
 }
