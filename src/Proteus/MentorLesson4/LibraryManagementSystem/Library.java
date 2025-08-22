c6a777d3efce7dd955a576eef8f71dcfc73ff828
eljan_abdullazada <a3371977@gmail.com>
2025-08-22 04:17:37 +0400

    complete Library class

--- src/Proteus/MentorLesson4/LibraryManagementSystem/Library.java
+++ src/Proteus/MentorLesson4/LibraryManagementSystem/Library.java
@@ -1,5 +1,37 @@
 package Proteus.MentorLesson4.LibraryManagementSystem;
 
+import java.util.ArrayList;
+
 public class Library {
-    
+    private String name;
+    private ArrayList books;
+    private Book book;
+
+    public String addBook(Book book){
+        books.add(book);
+        return "Book " + book.getTitle() + " by " + book.getAuthor() + " is added to the library";
+    }
+
+    public void borrowBook(String title) throws BookAlreadyBorrowedException{
+        if(books.contains(title)){
+            book.borrowBook(title, book.getAuthor());
+        }
+    }
+
+    public void returnBook(String title) throws BookNotBorrowedException{
+        book.returnBook(title, book.getAuthor());
+    }
+
+    public String printAllBooks(){
+        return books.toString();
+    }
+
+
+    public ArrayList getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList books) {
+        this.books = books;
+    }
 }
