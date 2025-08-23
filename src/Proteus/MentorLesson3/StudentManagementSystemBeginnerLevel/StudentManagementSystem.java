3c1fd8b7b6bc53c72643c7bacced8731524dd58f
eljan_abdullazada <a3371977@gmail.com>
2025-08-24 00:02:36 +0400

    StudentManagementSystem partially complete

---
+++ src/Proteus/MentorLesson3/StudentManagementSystemBeginnerLevel/StudentManagementSystem.java
@@ -0,0 +1,53 @@
+package Proteus.MentorLesson3.StudentManagementSystemBeginnerLevel;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class StudentManagementSystem {
+    private static List<Student> studentList;
+    Scanner scanner = new Scanner(System.in);
+
+    public  static void main(String [] args){
+
+    }
+
+    public static void addStudent(Student student) throws StudentNotFoundException{
+        studentList.add(student);
+        System.out.println(student);
+    }
+
+    static void showStudents(){
+        for(Student student: studentList){
+            System.out.println(student);
+        }
+    }
+
+    public static void deleteStudent(int id){
+        for(Student student: studentList){
+            if(student.getId() == id){
+                studentList.remove(student);
+                System.out.println("Student with id " + id + " deleted successfully");
+            }
+            else{
+                throw new StudentNotFoundException("Student with this ID does not exist.");
+            }
+        }
+    }
+
+    public static void updateStudent(int id, String name){
+        for(Student student: studentList){
+            if(student.getId() == id){
+                System.out.println("Student name" + student.getName() + " with ID " + id + " is changed to -> " + name);
+                student.setName(name);
+            }
+            else{
+                throw new StudentNotFoundException("Student with this ID does not exist.");
+            }
+        }
+    }
+
+    public static Student getStudentById(int id){
+
+    }
+
+}
