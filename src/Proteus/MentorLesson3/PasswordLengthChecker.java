9f85b06ef0f680e72019fc87f14326b1acaa3d99
eljan_abdullazada <a3371977@gmail.com>
2025-08-29 22:28:23 +0400

    start password checker

---
+++ src/Proteus/MentorLesson3/PasswordLengthChecker.java
@@ -0,0 +1,11 @@
+package Proteus.MentorLesson3;
+
+import java.util.Scanner;
+
+public class PasswordLengthChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
