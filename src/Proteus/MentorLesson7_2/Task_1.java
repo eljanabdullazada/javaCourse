09fc33e0d70f6a86b54edbc1900a809710d29c34
eljan_abdullazada <a3371977@gmail.com>
2025-09-03 02:57:44 +0400

    lesson 7 in class task 1 complete

---
+++ src/Proteus/MentorLesson7_2/Task_1.java
@@ -0,0 +1,33 @@
+package Proteus.MentorLesson7_2;
+
+import java.util.ArrayList;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        ArrayList<String> goods = new ArrayList<>();
+        goods.add("apple");
+        goods.add("pineapple");
+        goods.add("banana");
+        goods.add("cherry");
+        goods.add("plum");
+
+        System.out.println(goods.get(0));
+        System.out.println(goods.get(goods.size() - 1));
+        System.out.println();
+
+        System.out.println(goods.remove(2));
+
+        for (String product: goods){
+            System.out.println(product);
+        }
+
+        System.out.println();
+
+        goods.set(0, "watermelon");
+
+        for (String product: goods){
+            System.out.println(product);
+        }
+
+    }
+}
