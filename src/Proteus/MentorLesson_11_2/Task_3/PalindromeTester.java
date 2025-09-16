c565d36e8023b1850be43c72bb391ee277607c76
eljan_abdullazada <a3371977@gmail.com>
2025-09-16 20:27:59 +0400

    Practise Palindrome with StringBuilder

---
+++ src/Proteus/MentorLesson_11_2/Task_3/PalindromeTester.java
@@ -0,0 +1,17 @@
+package Proteus.MentorLesson_11_2.Task_3;
+
+import java.util.Scanner;
+public class PalindromeTester {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        StringBuilder sb = new StringBuilder(input);
+
+        if(input.equals(sb.reverse().toString())){
+            System.out.println("It is a Palindrome.");
+        } else{
+            System.out.println("It is not a Palindrome.");
+        }
+
+    }
+}
