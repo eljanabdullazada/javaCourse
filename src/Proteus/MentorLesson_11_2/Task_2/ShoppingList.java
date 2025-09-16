9b50a21a5191d1fbb34db78d7b312ad36f74a462
eljan_abdullazada <a3371977@gmail.com>
2025-09-16 20:21:01 +0400

    practise string builder, turn an array of elements into single string separated with commas

---
+++ src/Proteus/MentorLesson_11_2/Task_2/ShoppingList.java
@@ -0,0 +1,18 @@
+package Proteus.MentorLesson_11_2.Task_2;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        String []goods = {"Çörək", "Süd", "Yumurta", "Pendir", "Yağ"};
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < goods.length; i++){
+            sb.append(goods[i]);
+            if(i < goods.length - 1){
+                sb.append(", ");
+            }
+        }
+
+        String result = sb.toString();
+        System.out.println(result);
+    }
+}
