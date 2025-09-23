c6e84f801aaa15e10a09285330c2ddfc3a6560c1
eljan_abdullazada <a3371977@gmail.com>
2025-09-23 23:09:12 +0400

    modify Leetcode problem 217 to use Set instead of HashSet for reusability

--- src/Proteus/Leetcode/ContainsDuplicate217.java
+++ src/Proteus/Leetcode/ContainsDuplicate217.java
@@ -1,22 +1,25 @@
 package Proteus.Leetcode;
 
+import java.util.Set;
 import java.util.HashSet;
 
 public class ContainsDuplicate217 {
+
     public static void main(String[] args) {
         int [] nums = {1,2,3,1};
         System.out.println(containsDuplicate(nums));
     }
 
     public static boolean containsDuplicate(int[] nums) {
-        HashSet mySet = new HashSet();
+        // using set will allow us to later make the code reusable by adjusting only the initialized part
+        // such as using TreeSet or sth else which may sort the numbers or perform other operations.
+        Set<Integer> mySet = new HashSet();
         for(int i = 0; i < nums.length; i++){
             if(mySet.contains(nums[i])){
                 return true;
             }
             mySet.add(nums[i]);
         }
-
         return false;
     }
 }
