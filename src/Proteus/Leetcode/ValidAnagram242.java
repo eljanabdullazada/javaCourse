9c209cdc1353bf7333b51f9c88a91b562a2aa4ac
eljan_abdullazada <a3371977@gmail.com>
2025-09-25 18:04:47 +0400

    add better version of Valid Anagram with Log(n) time complexity as commented section

--- src/Proteus/Leetcode/ValidAnagram242.java
+++ src/Proteus/Leetcode/ValidAnagram242.java
@@ -31,3 +31,30 @@ public class ValidAnagram242 {
         return false;
     }
 }
+
+//public class ValidAnagram242 {
+//    public static void main(String[] args) {
+//        String s1 = "anagram", t1 = "nagaram";
+//        String s2 = "rat", t2 = "car";
+//
+//        System.out.println(isAnagram(s1, t1)); // true
+//        System.out.println(isAnagram(s2, t2)); // false
+//    }
+//
+//    public static boolean isAnagram(String s, String t) {
+//        if (s.length() != t.length()) return false;
+//
+//        int[] counts = new int[26]; // for lowercase a-z
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            counts[s.charAt(i) - 'a']++;  // increment count for s
+//            counts[t.charAt(i) - 'a']--;  // decrement count for t
+//        }
+//
+//        for (int count : counts) {
+//            if (count != 0) return false;
+//        }
+//
+//        return true;
+//    }
+//}
