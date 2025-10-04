cb2d41662b85ffa972295361cb99060a9ccd8e54
eljan_abdullazada <a3371977@gmail.com>
2025-10-04 19:09:29 +0400

    Modify Group Anagrams Leetcode problem 49

--- src/Proteus/Leetcode/GroupAnagrams49.java
+++ src/Proteus/Leetcode/GroupAnagrams49.java
@@ -3,19 +3,23 @@ package Proteus.Leetcode;
 import java.util.*;
 
 public class GroupAnagrams49 {
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-        for (String s : strs) {
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
-        }
-        return new ArrayList<>(map.values());
-    }
 
     public static void main(String[] args) {
-        String[] input = {"eat","tea","tan","ate","nat","bat"};
-        System.out.println(groupAnagrams(input));
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
     }
+
+    public static List<List<String>> groupAnagrams(String[] strs){
+
+        Map<String, List<String>> myMap = new HashMap<>();
+        for (String word: strs){
+            char[] arr = word.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            myMap.computeIfAbsent(key, k -> new ArrayList<>()).add(word);
+        }
+
+        return new ArrayList<>(myMap.values());
+    }
+
 }
