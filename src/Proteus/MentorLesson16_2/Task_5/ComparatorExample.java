773b047be30d448d8790d8b90fbf59235c879d5f
eljan_abdullazada <a3371977@gmail.com>
2025-10-17 04:49:11 +0400

    Mentor Lesson 16_2 task 6 complete

--- src/Proteus/MentorLesson16_2/Task_5/ComparatorExample.java
+++ src/Proteus/MentorLesson16_2/Task_5/ComparatorExample.java
@@ -1,14 +1,39 @@
 package Proteus.MentorLesson16_2.Task_5;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
+
+class Student {
+    String name;
+    int score;
+
+    Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + score + ")";
+    }
+}
 
 public class ComparatorExample {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Leyla", "Ali", "Zaur", "Emin");
-        names.sort(Comparator.comparingInt(String::length));
-        names.sort(Comparator.naturalOrder());
-        System.out.println(names);
+        List<Student> students = Arrays.asList(
+                new Student("Leyla", 60),
+                new Student("Ali", 45),
+                new Student("Zaur", 55),
+                new Student("Emin", 70)
+        );
+
+        Predicate<Student> highScore = s -> s.score > 50;
+        Predicate<Student> longName = s -> s.name.length() > 2;
+
+        students.forEach(s -> {
+            if (highScore.and(longName).test(s)) {
+                System.out.println("Passed: " + s);
+            }
+        });
     }
 }
